2e0047737760b0de4207547d213683a00dab0eab
Roman Morozov <roman.morozov@bsci.com>
2018-10-03 18:46:42 -0700

    init - personal

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi JAMstack</h1>
+    <p>
+      My name is <strong>Roman Morozov</strong> and I would like to participate
+      in the hackathon 3–4th of November 2018!
+    </p>
+    <p>Hopefully that's enough of personalization 😅</p>
+  </Layout>
+)
+
+export default IndexPage
